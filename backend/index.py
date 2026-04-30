8e00e31bab3832e2db7a7293610225ecee811cdc
Juan Jose Vasquez-Mesa <juanjosevasquezmesa@gmail.com>
2026-04-30 13:04:16 -0400

    Change the mock data

--- backend/index.py
+++ backend/index.py
@@ -6,4 +6,7 @@ index_bp = Blueprint("index", __name__)
 
 @index_bp.route("/")
 def index():
+    if session:
+        name = session.get("name")
+        nameList = name.split()
     return render_template("index.html", session=session)
